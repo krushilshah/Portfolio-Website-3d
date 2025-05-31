c5275786d3876b621a23845ba93a989c1def801b
krushil shah <krushilshah06@gmail.com>
2025-05-31 13:54:37 +0530

    first dd sss

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,7 +45,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          Logo
+          KARAN LAKHANI
         </a>
         <a
           href="mailto:example@mail.com"
