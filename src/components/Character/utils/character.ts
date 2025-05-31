5e2a42c35b78d3e698467ccf7922908a0c503a52
KRUSHIL SHAH <111522397+krushilshah@users.noreply.github.com>
2025-05-31 16:38:31 +0530

    Update character.ts

--- src/components/Character/utils/character.ts
+++ src/components/Character/utils/character.ts
@@ -17,7 +17,7 @@ const setCharacter = (
     return new Promise<GLTF | null>(async (resolve, reject) => {
       try {
         const encryptedBlob = await decryptFile(
-          "/models/character.enc",
+          "/models/characterd.enc",
           "Character3D#@"
         );
         const blobUrl = URL.createObjectURL(new Blob([encryptedBlob]));
